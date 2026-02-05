0961cdbc5aecf2be7b7f6322e3b2f8635b2b9795
Tushar Verma <231210117@nitdelhi.ac.in>
2026-02-06 02:05:35 +0530

    thirteenth

--- app/src/main/java/com/example/breify20/ui/screens/Profile.kt
+++ app/src/main/java/com/example/breify20/ui/screens/Profile.kt
@@ -1,8 +1,6 @@
 package com.example.breify20.ui.screens
 
-import InputField
 import ProfileField
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -17,18 +15,13 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -37,15 +30,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.example.breify20.R
 
 @Composable
 fun ProfileScreen(modifier: Modifier = Modifier , navController: NavController) {
@@ -78,7 +67,7 @@ fun ProfileScreen(modifier: Modifier = Modifier , navController: NavController) 
                 .padding(20.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-
+            Spacer(modifier = modifier.height(20.dp))
             Row(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.Start
